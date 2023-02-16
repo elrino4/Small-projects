f51b07d6fa62579ff471008305dc8910153b2f24
elrino4 <igorslarionovs27@gmail.com>
2023-02-16 21:09:40 +0100

    This is my first upload on GitHub

---
+++ project2/index.js
@@ -0,0 +1,8 @@
+// let overlay= document.getElementById("overlay")
+// let topPicture = document.getElementById("topPicture")
+
+// overlay.style.display="none"
+
+
+//  topPicture.addEventListener("mouseenter", () => overlay.style.display = "block")
+//  topPicture.addEventListener("mouseleave",()=> overlay.style.display = "none")
